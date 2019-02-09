453b07932931f03cae3d9ac8f0dbd18f86f74e00
Gateka <47472522+Gateka@users.noreply.github.com>
2019-02-09 13:38:19 +0200

    update

--- oop2coursework1.java
+++ oop2coursework1.java
@@ -5,4 +5,12 @@ public class oop2coursework1{
     Scanner arm = new Scanner(System.in);
     System.out.println("Enter Student's name: ");
     String nam = arm.next();
-    
+    int a=0,r=0,m=0,e=0,l=0,n=0,av=0;
+    for(;a <= 0 || a > 100;){
+    if(a == 0){
+    System.out.println("Enter First Courseunit name : ");
+      nam = arm.next();
+    }
+      System.out.print("Enter First Courseunit result: ");
+      a = arm.nextInt();
+    }
