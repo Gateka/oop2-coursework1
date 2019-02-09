45ae668f97a040124579687dbc093866920b53d3
Gateka <47472522+Gateka@users.noreply.github.com>
2019-02-09 12:14:57 +0200

    Update

--- oop2coursework1.java
+++ oop2coursework1.java
@@ -1,1 +1,8 @@
-
+package oop2coursework1;
+import java.util.Scanner;
+public class oop2coursework1{
+  public static void main(String[] args){
+    Scanner arm = new Scanner(System.in);
+    System.out.println("Enter Student's name: ");
+    String nam = arm.next();
+    
